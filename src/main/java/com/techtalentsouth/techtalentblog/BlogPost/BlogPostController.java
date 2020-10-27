fd52184771dc63afcc5981cae890d77f6df3acf1
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-10-26 21:32:29 -0500

    work from thursday night'

--- src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
@@ -23,6 +27,10 @@ public class BlogPostController {
 
     @GetMapping(value = "/")
     public String index(BlogPost blogpost, Model model){
+        posts.removeAll(posts);
+        for (BlogPost postFromDB : blogpostrepository.findAll()){
+            posts.add(postFromDB);
+        }
         model.addAttribute("posts", posts);
         return "blogpost/index";
     }
@@ -43,4 +51,12 @@ public class BlogPostController {
 
         return "blogpost/result";
     }
+
+    @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.DELETE)
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogpost){
+
+        blogpostrepository.deleteById(id);
+        return "/blogpost/index";
+    }
+
 }
